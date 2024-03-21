9a0c9abb8afdef658159e54be8b72c8e86af9989
Juan Martin <martinmuda@gmail.com>
2024-03-21 16:11:57 +0300

    fix script

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
-const { execSync } = require('child_process')
-const path = require('path')
+const { execSync } = require('child_process');
+const path = require('path');
+
 try {
-  const args = process.argv.slice(2).join(' ')
-  const configPath = path.resolve(__dirname, '../cliff.toml')
-  const mdPath = path.resolve(__dirname, '../CHANGELOG.md')
-  const result = execSync(`git cliff -c ${configPath} -o ${mdPath} -l ${args}`, {
-    stdio: 'inherit'
-  })
-  // -o shlud be at root project
-  // -u for unreleased
-  // -l for latest commit tag
-  // if (result === null) console.log('Lint SCSS sin errores!!')
+    const args = process.argv.slice(2).join(' ');
+    const configPath = path.resolve(__dirname, '../cliff.toml');
+    const mdPath = path.resolve(__dirname, '../CHANGELOG.md');
+    const result = execSync(`git cliff -c ${configPath} -o ${mdPath} ${args}`);
+    
+    // -o should be at root project
+    // -u for unreleased
+    // -l for latest commit tag
+    console.log(result.toString()); // Print output of the command
 } catch (error) {
-  console.log('Git cliff has errors')
+    console.log('Git cliff has errors');
 }
