b4ad62516354b1f6ac4914eace7bb87289c7e3b1
Juan Martin <martinmuda@gmail.com>
2024-03-21 16:16:23 +0300

    fix: execSync

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 const { execSync } = require('child_process');
 const path = require('path');
 
@@ -6,12 +7,9 @@ try {
     const args = process.argv.slice(2).join(' ');
     const configPath = path.resolve(__dirname, '../cliff.toml');
     const mdPath = path.resolve(__dirname, '../CHANGELOG.md');
-    const result = execSync(`git cliff -c ${configPath} -o ${mdPath} ${args}`);
-    
-    // -o should be at root project
-    // -u for unreleased
-    // -l for latest commit tag
-    console.log(result.toString()); // Print output of the command
+    execSync(`git cliff -c ${configPath} -o ${mdPath} ${args}`, {
+        stdio: 'inherit' // Redirect input/output/error to the parent process
+    });
 } catch (error) {
     console.log('Git cliff has errors');
 }
