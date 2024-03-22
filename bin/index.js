56eb12d97d2387c4261f7731671d0f3d9a6cb80e
Juan Martin <martinmuda@gmail.com>
2024-03-22 13:00:59 +0300

    Fix header and tag in changelog custom script

--- bin/index.js
+++ bin/index.js
@@ -8,75 +8,80 @@ try {
   const args = process.argv.slice(2).join(' ')
   const configPath = path.resolve(__dirname, '../cliff.toml')
 
-  function generateChangelog() {
-    let latestTag
+
+
+function generateChangelog() {
+    let latestTag;
     try {
-      latestTag = execSync('git describe --tags --abbrev=0').toString().trim()
+        latestTag = execSync('git describe --tags --abbrev=0').toString().trim();
     } catch (error) {
-      // No hay tags, asumir que es el primer release
-      latestTag = ''
+        // No hay tags, asumir que es el primer release
+        latestTag = '';
     }
 
-    let commits
+    let commits;
     if (latestTag) {
-      commits = execSync(`git log --pretty=format:'%s' ${latestTag}..HEAD`)
-        .toString()
-        .trim()
-        .split('\n')
+        commits = execSync(`git log --pretty=format:'%s %H %ad' ${latestTag}..HEAD`).toString().trim().split('\n');
     } else {
-      commits = execSync('git log --pretty=format:"%s"')
-        .toString()
-        .trim()
-        .split('\n')
+        commits = execSync('git log --pretty=format:"%s %H %ad"').toString().trim().split('\n');
     }
 
-    let groupedCommits = {}
+    let groupedCommits = {};
 
-    commits.forEach((commit) => {
-      const [type, ...description] = commit.split(':')
-      const trimmedType = type.trim()
+    commits.forEach(commit => {
+        const [message, hash, date] = commit.split(' ');
 
-      if (trimmedType in groupedCommits) {
-        groupedCommits[trimmedType].push(description.join(':').trim())
-      } else {
-        groupedCommits[trimmedType] = [description.join(':').trim()]
-      }
-    })
+        const [type, ...description] = message.split(':');
+        const trimmedType = type.trim();
 
-    let changelog = ''
+        if (trimmedType in groupedCommits) {
+            groupedCommits[trimmedType].push({ description: description.join(':').trim(), hash, date });
+        } else {
+            groupedCommits[trimmedType] = [{ description: description.join(':').trim(), hash, date }];
+        }
+    });
 
-    Object.keys(groupedCommits).forEach((type) => {
-      changelog += `### ${type}\n\n`
-      groupedCommits[type].forEach((description) => {
-        changelog += `- ${description}\n`
-      })
-      changelog += '\n'
-    })
+    let changelog = '';
 
-    return changelog
-  }
+    if (latestTag) {
+        changelog += `## Cambios desde el tag ${latestTag}\n\n`;
+    } else {
+        changelog += `## Cambios desde el inicio del proyecto\n\n`;
+    }
 
-  function updateChangelog(changelog) {
-    let existingChangelog = ''
+    Object.keys(groupedCommits).forEach(type => {
+        changelog += `### ${type}\n\n`;
+        groupedCommits[type].forEach(commit => {
+            changelog += `- ${commit.description} (${commit.hash}) - ${commit.date}\n`;
+        });
+        changelog += '\n';
+    });
+
+    return changelog;
+}
+
+function updateChangelog(changelog) {
+    let existingChangelog = '';
 
     try {
-      existingChangelog = fs.readFileSync('CHANGELOG.md', 'utf8')
+        existingChangelog = fs.readFileSync('CHANGELOG.md', 'utf8');
     } catch (err) {
-      // No existe un changelog, se creará uno nuevo
+        // No existe un changelog, se creará uno nuevo
     }
 
     if (existingChangelog) {
-      // Se agrega el nuevo changelog al inicio
-      fs.writeFileSync('CHANGELOG.md', changelog + '\n' + existingChangelog)
+        // Se agrega el nuevo changelog al inicio
+        fs.writeFileSync('CHANGELOG.md', changelog + '\n' + existingChangelog);
     } else {
-      // Se crea un nuevo changelog
-      fs.writeFileSync('CHANGELOG.md', changelog)
+        // Se crea un nuevo changelog
+        fs.writeFileSync('CHANGELOG.md', changelog);
     }
-  }
+}
 
-  const newChangelog = generateChangelog()
-  updateChangelog(newChangelog)
-  console.log('Changelog generado o actualizado correctamente.')
+const newChangelog = generateChangelog();
+updateChangelog(newChangelog);
+console.log('Changelog generado o actualizado correctamente.');
+
 } catch (error) {
   console.log('Changelog has errors')
   process.exit(1)
