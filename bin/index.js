e6f3d160b45c8f980069966186acad5d874f024f
Juan Martin <martinmuda@gmail.com>
2024-03-22 11:27:25 +0300

    fix changelog output

--- bin/index.js
+++ bin/index.js
@@ -6,9 +6,10 @@ const path = require('path');
 try {
     const args = process.argv.slice(2).join(' ');
     const configPath = path.resolve(__dirname, '../cliff.toml');
-    execSync(`git cliff -c ${configPath} -o ./CHANGELOG.md ${args}`, {
+    execSync(`git cliff -c ${configPath} --prepend ./CHANGELOG.md ${args}`, {
         stdio: 'inherit' // Redirect input/output/error to the parent process
     });
 } catch (error) {
     console.log('Git cliff has errors');
+    process.exit(1);
 }
