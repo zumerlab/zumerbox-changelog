19d0d7ead6844c89a3b4fa2efb83c7e6745672af
Juan Martin <martinmuda@gmail.com>
2024-03-25 09:41:36 +0300

    Add option to hide empty releases

--- bin/index.js
+++ bin/index.js
@@ -7,7 +7,7 @@ const fs = require('fs')
 try {
   const args = process.argv.slice(2).join(' ')
   const configPath = path.resolve(__dirname, '../compact-tinybox.hbs') 
-  execSync(`auto-changelog -p -t ${configPath} --commit-limit false --hide-credit ${args}`, {
+  execSync(`auto-changelog -p -t ${configPath} --commit-limit false --hide-empty-releases --hide-credit ${args}`, {
     stdio: 'inherit'
   })
 
